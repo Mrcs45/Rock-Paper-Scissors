da8c9bebc0dc262d8b97a16f46df1b940eed80fe
Marcos Neto <marcos.msyu@outlook.com>
2025-02-10 14:29:00 -0300

    Added the playGame function and finished the job!
    The game is now fully working trough the console

--- src/script.js
+++ src/script.js
@@ -29,31 +29,49 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-/* Write the logic to play a single round */
+/* Write the logic to play a entire game */
 
-/* Create a function named playRound with two parameters humanChoice and computerChoice */
-function playRound(humanChoice, computerChoice) {
-/* Make humanchoice case-insensitive */
-    let lower = humanChoice.toLowerCase();
-    /* Write the code for the round winner such as: "You lose! Paper beats Rock" */
-    if (lower === computerChoice) {
-        return "It's a tie!"
-    } 
-    if (
-        (lower === 'rock' && computerChoice === 'scissors') ||
-        (lower === 'paper' && computerChoice === 'rock') ||
-        (lower === 'scissors' && computerChoice === 'paper')
-    ) {
-        humanScore++
-        return `You win! ${lower} beats ${computerChoice}.`;
-    } else {
-        computerScore++
-        return `You Lose! ${computerChoice} beats ${lower}`;
-    }
+/* Create the function named playGame */
+function playGame() {
+    /* Move playRoound function inside the playGame */
+
+    /* Play 5 round by calling playRound 5 times */
+        while (humanScore < 5 && computerScore < 5) {
+            playRound(getHumanChoice(), getcomputerChoice());
+        }
+    /* Write the logic to play a single round */
     
-}
-/* console.log(playRound(getHumanChoice(), getcomputerChoice()));
-console.log(humanScore);
-console.log(computerScore); */
+    /* Create a function named playRound with two parameters humanChoice and computerChoice */
+    function playRound(humanChoice, computerChoice) {
+    /* Make humanchoice case-insensitive */
+        let lower = humanChoice.toLowerCase();
+        /* Write the code for the round winner such as: "You lose! Paper beats Rock" */
+        if (lower === computerChoice) {
+            return "It's a tie!"
+        } 
+        if (
+            (lower === 'rock' && computerChoice === 'scissors') ||
+            (lower === 'paper' && computerChoice === 'rock') ||
+            (lower === 'scissors' && computerChoice === 'paper')
+        ) {
+            humanScore++
+            return `You win the round! ${lower} beats ${computerChoice}.`;
+        } else {
+            computerScore++
+            return `You Lose the round! ${computerChoice} beats ${lower}`;
+        }
+        
+    }
+        /* Checks who win the game */
+        if (humanScore = 5) {
+            return `Congratulations! You won the game! your score: ${humanScore} computer score: ${computerScore}`;
+        } 
+        if (computerScore = 5) {
+            return `You lose! The computer won the game! your score: ${humanScore} computer score: ${computerScore}`;
+            
+        };
+};
 
-
+    /* Call the game function */
+    console.log(playGame());
+    
